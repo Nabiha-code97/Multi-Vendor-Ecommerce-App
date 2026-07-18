9955c8c16f04c417b2359b84166d6e24aecdbd1f
Nabiha-code97 <nabihaamir97@gmail.com>
2026-07-18 11:23:51 +0500

    Frontend setup with signup and login pages

---
+++ client/src/AppRoutes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import App from './App'
+import Home from './Home'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+
+export default function AppRoutes() {
+  return (
+    <>
+    <Routes>
+    <Route path='/' element={<App/>} />
+    <Route path='/login' element={<LoginPage/>} />
+    <Route path='/sign-up' element={<SignUpPage/>} />
+    </Routes>
+    </>
+  )
+}
---
+++ client/src/pages/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignUp from '../components/SignUp'
+
+
+export default function SignUpPage() {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  )
+}
