95edfcc3f4e37f2a33683d1b4eb1e423e5cfb79a
Nabiha-code97 <nabihaamir97@gmail.com>
2026-07-30 21:11:15 +0500

    Add account activation page

--- client/src/AppRoutes.jsx
+++ client/src/AppRoutes.jsx
@@ -4,6 +4,7 @@ import App from './App'
 import Home from './Home'
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
+import ActivationPage from './pages/ActivationPage'
 
 export default function AppRoutes() {
   return (
@@ -12,6 +13,7 @@ export default function AppRoutes() {
     <Route path='/' element={<App/>} />
     <Route path='/login' element={<LoginPage/>} />
     <Route path='/sign-up' element={<SignUpPage/>} />
+    <Route path='/activation/:activationToken' element={<ActivationPage/>}/>
     </Routes>
     </>
   )
